dd5594c399b372bb8aec1562298ea256d5aa14f8
Mikhail Trapeznikov <trapma@yandex.ru>
2022-07-05 04:17:53 +1000

    create App with components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
+import React, { useState } from "react";
+import PostList from "./components/PostList";
+import "./style/app.css";
+// import Counter from "./components/Counter";
+// import Input from "./components/Input";
+// import ClassCounter from "./components/oldComponents/classCounter";
 
 function App() {
+  const [posts, setPost] = useState([
+    { id: 1, title: "JavaScript", body: "description" },
+    { id: 2, title: "JavaScript 2", body: "description" },
+    { id: 3, title: "JavaScript 3", body: "description" },
+  ]);
+  const [posts2, setPost2] = useState([
+    { id: 1, title: "Python", body: "description" },
+    { id: 2, title: "Python 2", body: "description" },
+    { id: 3, title: "Python 3", body: "description" },
+  ]);
   return (
     <div className="App">
+      <PostList posts={posts} title={"Посты про JS"} />
+      <PostList posts={posts2} title={"Посты про Python"} />
     </div>
   );
 }
